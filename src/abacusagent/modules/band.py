35af9287a7730fd7e25ddd21ac071884448da3b7
ahxbcn <zhuhong850@gmail.com>
2025-06-17 15:11:53 +0800

    plot band using hybrid density functional using PYATB

--- src/abacusagent/modules/band.py
+++ src/abacusagent/modules/band.py
@@ -2,7 +2,7 @@ import os
 import shutil
 import time
 from pathlib import Path
-from typing import Literal, Optional, TypedDict, Dict, Any
+from typing import Literal, Optional, TypedDict, Dict, Any, List
 from abacustest.lib_prepare.abacus import AbacusStru, ReadInput, WriteInput
 from abacustest.lib_model.model_013_inputs import PrepInput
 
@@ -325,13 +325,14 @@ def abacus_plot_band(band_work_path,
                 plt.plot(kline_splited[j], bands_dw_splited[i][j], 'b--', linewidth=1.0)
     plt.xlim(kline[0], kline[-1])
     plt.ylim(-energy_range, energy_range)
-    plt.ylabel(r"$E-E_\text{fermi}$/eV")
+    plt.ylabel(r"$E-E_\text{F}$/eV")
     plt.xticks(high_symm_poses, high_symm_labels)
     plt.grid()
     if nspin == 1:
         plt.title(f"Band structure of {material_name} (Gap = {band_gap:.2f} eV)")
     elif nspin == 2:
         plt.title(f"Band structure of {material_name}\n(Up spin gap = {band_gap:.2f} eV, down spin gap = {band_gap_dw:.2f} eV)")
+    plt.legend()
     plt.savefig(band_work_path / band_pic_path, dpi=300)
     plt.show()
 
@@ -339,6 +340,150 @@ def abacus_plot_band(band_work_path,
         return band_gap
     else:
         return [band_gap, band_gap_dw]
+
+def abacus_cal_band_scf_pyatb(stru_file,
+                               stru_type,
+                               lcao,
+                               band_work_path,
+                               dft_functional = 'hse',
+                               kspacing = 0.16,
+                               scf_thr = 1e-6,
+                               extra_input = None):
+    """
+    Do scf calculation using hybrid functionals on a uniform k-mesh.
+    Used by calculating hybrid density functional band by pyatb.
+    """
+    pp_path = os.environ.get("ABACUS_PP_PATH")
+    orb_path = os.environ.get("ABACUS_ORB_PATH")
+
+    extra_input['kspacing'] = kspacing
+    extra_input['dft_functional'] = dft_functional
+    extra_input['scf_thr'] = scf_thr
+    extra_input['out_mat_hs2'] = True
+    extra_input['out_mat_r']   = True
+
+    extra_input_file = "INPUT.tmp"
+    WriteInput(extra_input, extra_input_file)
+
+    try:
+        _, job_path = PrepInput(files=str(stru_file),
+                                filetype=stru_type,
+                                jobtype='scf',
+                                pp_path=pp_path,
+                                orb_path=orb_path,
+                                input_file=extra_input_file,
+                                lcao=lcao
+                                ).run()
+    except Exception as e:
+        raise RuntimeError(f"Error preparing input files: {e}")
+    
+    old_job_path = job_path[0]
+    band_scf_work_path = (band_work_path / "scf").absolute()
+    shutil.move(old_job_path, band_scf_work_path)
+
+    run_abacus_onejob(band_scf_work_path)
+    scf_metrics = abacus_collect_data(band_scf_work_path, ['normal_end'])['collected_metrics']
+
+    if scf_metrics['normal_end'] is True:
+        return True
+    else:
+        return False
+
+def abacus_postprocess_band_pyatb(nspin,
+                                  band_work_path,
+                                  band_pdf_path,
+                                  energy_range = 10,
+                                  connect_line_points=30):
+    """
+    Post process SCF data and plot band using pyatb. 
+    This function is used for hybrid density functional band calculation as a workaround.
+    """
+    pyatb_process_dir = Path(band_work_path) / 'pyatb'
+    os.system(f"mkdir {pyatb_process_dir}")
+
+    scf_dir = (band_work_path / "scf").absolute()
+    scf_metrics = abacus_collect_data(scf_dir, ['efermi', 'cell'])['collected_metrics']
+
+    input_parameters = {
+        'nspin': nspin,
+        'package': "ABACUS",
+        'fermi_energy': scf_metrics['efermi'],
+        'HR_route': "../scf/OUT.ABACUS/data-HR-sparse_SPIN0.csr",
+        'SR_route': "../scf/OUT.ABACUS/data-SR-sparse_SPIN0.csr",
+        'rR_route': "../scf/OUT.ABACUS/data-rR-sparse.csr",
+        "HR_unit":  "Ry",
+        "rR_unit": "Bohr"
+    }
+    if nspin == 2:
+        input_parameters['HR_route'] += ' ../scf/OUT.ABACUS/data-HR-sparse_SPIN1.csr'
+        input_parameters['SR_route'] += ' ../scf/OUT.ABACUS/data-SR-sparse_SPIN1.csr'
+
+    pyatb_input_file = open((pyatb_process_dir / "Input").absolute(), "w")
+    
+    pyatb_input_file.write("INPUT_PARAMETERS\n{\n")
+    for key, value in input_parameters.items():
+        pyatb_input_file.write(f"    {key}  {value}\n")
+    pyatb_input_file.write("}\n\nLATTICE\n{\n")
+
+    pyatb_input_file.write(f"    {'lattice_constant'}  {1.8897162}\n")
+    pyatb_input_file.write(f"    {'lattice_constant_unit'}  {'Bohr'}\n    lattice_vector\n")
+    for cell_vec in scf_metrics['cell']:
+        pyatb_input_file.write(f"    {cell_vec[0]:.8f}  {cell_vec[1]:.8f}  {cell_vec[2]:.8f}\n")
+    pyatb_input_file.write("}\n\nBAND_STRUCTURE\n{\n    kpoint_mode   line\n")
+
+    # Get kline and write to pyatb Input file
+    shutil.copy(band_work_path / "scf" / "STRU", pyatb_process_dir / "STRU")
+    scf_stru = AbacusStru.ReadStru(band_work_path / "scf" / "STRU")
+    kpt_file = pyatb_process_dir / "KPT"
+    scf_stru.get_kline_ase(point_number=connect_line_points,kpt_file=kpt_file)
+
+    kpt_file_content = []
+    with open(kpt_file) as fin:
+        for lines in fin:
+            words = lines.split()
+            kpt_file_content.append(words)
+    
+    high_symm_nums = int(kpt_file_content[1][0])
+    kpoint_label = ''
+    for linenum in range(3, 3+high_symm_nums):
+        kpoint_label += kpt_file_content[linenum][-1]
+        if linenum < 2+high_symm_nums:
+            kpoint_label += ", "
+    pyatb_input_file.write(f"    kpoint_num    {high_symm_nums}\n")
+    pyatb_input_file.write(f"    kpoint_label  {kpoint_label}\n    high_symmetry_kpoint\n")
+    for linenum in range(3, 3+high_symm_nums):
+        kpoint_coord = f"    {kpt_file_content[linenum][0]} {kpt_file_content[linenum][1]} {kpt_file_content[linenum][2]}"
+        kline_num = f" {kpt_file_content[linenum][3]}\n"
+        pyatb_input_file.write(kpoint_coord + kline_num)
+    pyatb_input_file.write("}\n")
+
+    pyatb_input_file.close()
+
+    # Use pyatb to plot band
+    os.chdir(pyatb_process_dir)
+    os.system("OMP_NUM_THREADS=1 pyatb")
+
+    # read band gap
+    band_gaps = []
+    with open(pyatb_process_dir / "Out/Band_Structure/band_info.dat") as fin:
+        for lines in fin:
+            if "Band gap" in lines:
+                band_gaps.append(float(lines.split()[-1]))
+    
+    # Modify auto generated plot_band.py and replot the band
+    os.chdir(pyatb_process_dir / "Out/Band_structure")
+    os.system(f'sed -i "16c y_min = {-energy_range} # eV" plot_band.py')
+    os.system(f'sed -i "17c y_max =  {energy_range} # eV" plot_band.py')
+    os.system("python plot_band.py")
+    os.chdir(pyatb_process_dir)
+    
+    # Copy plotted band.pdf to given directory
+    os.system(f"cp {pyatb_process_dir / 'Out' / 'Band_Structure' / 'band.pdf'} {band_work_path / band_pdf_path}")
+
+    if nspin == 1:
+        return band_gaps[0]
+    else:
+        return [band_gaps[0], band_gaps[1]]
 
 @mcp.tool()
 def abacus_cal_band(stru_file: str,
@@ -348,32 +493,33 @@ def abacus_cal_band(stru_file: str,
                     band_pic_path: str = "band.png",
                     energy_range: float = 10.0,
                     lcao: bool = True,
-                    extra_input: Optional[Dict[str, Any]] = {}
+                    extra_input: Optional[Dict[str, Any]] = None
 ) -> TypedDict("results", {"band_gap": float, "band_picture": str}):
     """
     Calculate the electronic band of given material.
     If a band calculation is requested, this function should be used directly.
     For GGA (and rarely used LDA) DFT bands, the calculation will calculate band using converged charge of the
     same given density functional (default is PBE, specified by `dft_functional` in extra_input).
-    For hybrid DFT bands, the calculation uses converged GGA DFT charge (default is PBE) in nscf calculation, and the
-    density functional name should be specified using hybrid_dft_functional.
+    For hybrid DFT bands, this function should not be used.
     Args:
         stru_file: Structure file in cif, poscar, or abacus/stru format.
         stru_type: Type of structure file, can be 'cif', 'poscar', or 'abacus/stru'. 'cif' is the default. 'poscar' is the VASP POSCAR format. 'abacus/stru' is the ABACUS structure format.
         material_name: Name of the material, used in the title of plotted band
         nspin: Type of spin polarization. 1 means no spin polarization, 2 means collinear spin polarization, and 4 means noncollinear spin polarization. Currently only nspin=1 and 2 are supported.
-        band_pic_path: Path to save plotted band.
+        band_pic_path: Path to save picture of plotted band.
         energy_range: Set [-energy_range, energy_range] for $E-E_\text{fermi}$ in the plotted band. The default 10 eV is OK.
-        lcao: Whether to use LCAO basis set, default is True. If True, the orbital library path must be provided.
+        lcao: Whether to use LCAO basis set, default is True.
         extra_input: Extra input parameters for ABACUS. 
     Returns:
-        The bandgap of the structure and path to the plotted band.
+        The bandgap (in eV) of the structure and path to the plotted band.
     
     Raises:
         NotImplementedError: If nspin=4 is requested
     """
     if nspin not in extra_input.keys():
-        extra_input['nspin'] = nspin
+        extra_input['nspin'] = nspin    
+    if nspin == 2 or nspin == 4:
+        extra_input['mixing_beta'] = 0.4
     
     # Create work dir for band calculation
     abacusagent_work_path = Path(os.environ.get("ABACUSAGENT_WORK_PATH")).absolute()
@@ -409,23 +555,81 @@ def abacus_cal_band(stru_file: str,
     else:
         raise NotImplementedError("Band plot for nspin=4 is not supported yet")
 
+@mcp.tool()
+def abacus_cal_band_pyatb(stru_file: str,
+                          stru_type: Literal["cif", "poscar", "abacus/stru"] = "cif",
+                          nspin: Optional[Literal[1, 2, 4]] = 1,
+                          hybrid_dft: Optional[str] = 'hse',
+                          kspacing: Optional[float] = 0.16,
+                          scf_thr: Optional[float] = 1e-6,
+                          band_pdf_path: str = "band.pdf",
+                          energy_range: float = 10.0,
+                          lcao: bool = True,
+                          extra_input: Optional[Dict[str, Any]] = None
+) -> TypedDict("results", {"band_gap": List[float], "band_picture": str}):
+    """
+    Calculate band using hybrid functional using ABACUS to do SCF calculation and using pyatb to postprocess. 
+    Implemented for calculate band using hybrid functionals as a workaround. Should be used only if a hybrid functional is 
+    requested to calculate the band.
+    Args:
+        stru_file: Structure file in cif, poscar, or abacus/stru format.
+        stru_type: Type of structure file, can be 'cif', 'poscar', or 'abacus/stru'. 'cif' is the default. 'poscar' is the VASP POSCAR format. 'abacus/stru' is the ABACUS structure format.
+        nspin: Type of spin polarization. 1 means no spin polarization, 2 means collinear spin polarization, and 4 means noncollinear spin polarization. Currently only nspin=1 and 2 are supported.
+        hybrid_dft: Hybrid dft functional to be used to calculate the band. For example, `hse` means using HSE06.
+        kspacing: The `kspacing` in INPUT file used in SCF calculation. Can be changed to 0.18 or 0.20 if the system is too large.
+        band_pdf_path: Path to save plotted band. Currently only pdf format are supported.
+        energy_range: Set [-energy_range, energy_range] for $E-E_\text{F}$ in the plotted band. The default 10 eV is generally OK.
+        lcao: Whether to use LCAO basis set, default is True.
+        extra_input: Extra input parameters for ABACUS. 
+    Returns:
+        The bandgap (in eV) of the structure and path to the plotted band. Contains only 1 value for nspin=1, and 2 values (for up and down spin respectively) for nspin=2.
+    
+    Raises:
+        NotImplementedError: If nspin=4 is requested
+    """
+    if nspin not in extra_input.keys():
+        extra_input['nspin'] = nspin
+    
+    if nspin == 2 or nspin == 4:
+        extra_input['mixing_beta'] = 0.4
+    
+    # Create work dir for band calculation
+    abacusagent_work_path = Path(os.environ.get("ABACUSAGENT_WORK_PATH")).absolute()
+    band_work_path = abacusagent_work_path / f"./band-{time.strftime('%Y%m%d%H%M%S')}"
+    if not os.path.exists(band_work_path):
+        os.mkdir(band_work_path)
+    
+    scfrun_state = abacus_cal_band_scf_pyatb(stru_file,
+                                             stru_type,
+                                             lcao,
+                                             band_work_path,
+                                             dft_functional=hybrid_dft,
+                                             kspacing=kspacing,
+                                             scf_thr=scf_thr,
+                                             extra_input=extra_input)
+    if scfrun_state is False:
+        raise RuntimeError("SCF step in band calculation failed")
+    
+    band_gap = abacus_postprocess_band_pyatb(nspin,
+                                             band_work_path,
+                                             band_pdf_path,
+                                             energy_range)
+    
+    return {'band_gap': band_gap,
+            'band_pdf_path': band_pdf_path}
+
 if __name__ == '__main__':
     from abacusagent.env import set_envs
     set_envs()
 
-    stru_file = '/mnt/e/temp/NiO-opted.STRU'
-    stru_type = 'abacus/stru'
-    extra_input = {'dft_plus_u': 1,
-                   'orbital_corr': '2 -1',
-                   'hubbard_u': '7.0 0.0',
-                   'kspacing': 0.35}
+    stru_file = '/mnt/e/temp/Si_mp-149_primitive.cif'
+    stru_type = 'cif'
+    extra_input = {}
 
-    abacus_cal_band(stru_file,
-                    stru_type,
-                    2,
-                    'NiO',
-                    "band.png",
-                    10,
-                    True,
-                    extra_input)
-    
+    abacus_cal_band_pyatb(stru_file,
+                          stru_type,
+                          1,
+                          'hse',
+                          0.16,
+                          1e-3,
+                          extra_input=extra_input)
