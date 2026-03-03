60b0ebb99dfdaf56ca65813e91ebfa15c2d5ce95
ahxbcn <zhuhong850@gmail.com>
2026-03-03 13:48:59 +0800

    refactor: reformat tool to build slab

--- src/abacusagent/modules/submodules/structure_editor.py
+++ src/abacusagent/modules/submodules/structure_editor.py
@@ -22,7 +22,7 @@ def build_slab(stru_file: Path,
         miller_indices (Tuple[int, int, int]): Miller indices of the surface. Defaults to (1, 0, 0), which means (100) surface of the structure.
         layers (int, optional): Number of layers of the surface. Note that the layers is number of equivalent layers, not number of layers of atoms. Defaults to 3.
         surface_supercell (Tuple[int, int], optional): Supercell size of the surface. Default is (1, 1), which means no supercell.
-        vacuum (float, optional): Vacuum space between the cleaved surface and its periodic image. The total vacuum size will be twice this value. Units in Angstrom. Defaults to 15.0. 
+        vacuum (float, optional): Vacuum space between the cleaved surface and its periodic image. The total vacuum size will be twice this value. Units in Angstrom. Defaults to 15.0.
         vacuum_direction (Literal['a', 'b', 'c']): The direction of the vacuum space. Defaults to 'b'.
     Returns:
         A dictionary containing the path to the surface structure file.
@@ -31,20 +31,26 @@ def build_slab(stru_file: Path,
     Raises:
         ValueError: If stru_type is not supported.
     """
-    if stru_type == 'abacus/stru':
+    if stru_type == "abacus/stru":
         stru = AbacusSTRU.read(stru_file, fmt="stru")
         stru_ase = stru.to("ase")
-    elif stru_type in ['cif', 'poscar']:
+    elif stru_type in ["cif", "poscar"]:
         stru_ase = read(stru_file, format=stru_type)
     else:
         raise ValueError(f"Unsupported structure file type: {stru_type}")
-    
-    stru_surface = surface(stru_ase, miller_indices, layers, vacuum=vacuum/2, periodic=True)
-    stru_surface = make_supercell(stru_surface, [[surface_supercell[0], 0, 0], [0, surface_supercell[1], 0], [0, 0, 1]])
-    stru_surface_abacusstru = AbacusSTRU.from_ase(stru_surface, metadata={
-                "lattice_constant": A2BOHR,
-                "atom_type": "cartesian",
-            })
+
+    stru_surface = surface(stru_ase, miller_indices, layers, vacuum=vacuum / 2, periodic=True)
+    stru_surface = make_supercell(
+        stru_surface,
+        [[surface_supercell[0], 0, 0], [0, surface_supercell[1], 0], [0, 0, 1]],
+    )
+    stru_surface_abacusstru = AbacusSTRU.from_ase(
+        stru_surface,
+        metadata={
+            "lattice_constant": A2BOHR,
+            "atom_type": "cartesian",
+        },
+    )
     stru_surface_abacusstru.sort()
     
     # Permute axis to set vacuum direction along given axis. The vacuum direction create by ase.build.surface is always along z axis.
