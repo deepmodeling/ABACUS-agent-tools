8c8975339bca38312691f26225bb0de2b4ecb5e8
ahxbcn <zhuhong850@gmail.com>
2026-03-04 19:04:48 +0800

    feature: get kpath from structure file

--- src/abacusagent/modules/symmetry.py
+++ src/abacusagent/modules/symmetry.py
@@ -1,7 +1,25 @@
 from pathlib import Path
-from typing import Dict, Any
+from typing import Dict, Any, Literal
 
 from abacusagent.init_mcp import mcp
+
+@mcp.tool()
+def get_high_symm_points_from_stru(stru_file: Path,
+                                   stru_type: Literal['cif', 'poscar', 'abacus/stru'] = 'cif'
+) -> Dict[str, Any]:
+    """
+    Get high symmetry points and kpath from structure file.
+    Args:
+        stru_file (Path): Absolute path to the structure file.
+        stru_type (Literal): Type of the structure file, can be 'cif', 'poscar', or 'abacus/stru'.
+    Returns:
+        A Dictionary with keys:
+        - high_symm_points: A list of dictionary, where the key is the label of the point, and the value is the fractional coordinates of the point.
+        - kpath: A list of tuples, where each tuple is a pair of labels of the two points in the kpath.
+    """
+    from abacusagent.modules.util.symmetry import get_high_symm_points_from_stru as _get_high_symm_points_from_stru
+
+    return _get_high_symm_points_from_stru(stru_file, stru_type)
 
 @mcp.tool()
 def get_high_symm_points_from_abacus_inputs_dir(abacusjob_dir: Path) -> Dict[str, Any]:
